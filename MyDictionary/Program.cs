43733d25e51b98eed9da1d0861c064afa90a71cb
GURKAN CALISIR <gurkan01c@gmail.com>
2021-01-24 22:22:20 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int> liste = new MyDictionary<int>();
+            liste.Add(16);
+            liste.Add(15);
+            liste.Add(14);
+            liste.Add(13);
+            liste.Add(12);
+            liste.Add(11);
+            Console.WriteLine(liste[0]);
+            Console.WriteLine(liste.Length);
+
+        }
+    }
+}
